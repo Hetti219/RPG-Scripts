d88a6488ecf475e148362fe8c87e4a3ece4dbf05
Sathika Hettiarachchi <89432618+Hetti219@users.noreply.github.com>
2021-12-30 21:37:43 +0530

    Initial Commit
    Contains C# Scripts for Unity RPG Project (basic)

---
+++ RPG Scripts/EquipmentManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static EquipmentManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    Equipment[] currentEquipment;
+    SkinnedMeshRenderer[] currentMeshes;
+
+    public delegate void OnEquipmetChanged(Equipment newItem, Equipment oldItem);
+    public OnEquipmetChanged onEquipmetChanged;
+
+    Inventory inventory;
+
+    void Start()
+    {
+        inventory = Inventory.instance;
+
+        int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
+        currentEquipment = new Equipment[numSlots];
+        currentMeshes = new SkinnedMeshRenderer[numSlots];
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int)newItem.equipSlot;
+
+        Equipment oldItem = null;
+
+        if (currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+        }
+
+        if (onEquipmetChanged != null)
+        {
+            onEquipmetChanged.Invoke(newItem, oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+    }
+
+    public void Unequip(int slotIndex)
+    {
+        if (currentEquipment[slotIndex] != null)
+        {
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.Add(oldItem);
+
+            if (onEquipmetChanged != null)
+            {
+                onEquipmetChanged.Invoke(null, oldItem);
+            }
+
+            currentEquipment[slotIndex] = null;
+        }
+    }
+
+    public void UnequipAll()
+    {
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            UnequipAll();
+    }
+}
